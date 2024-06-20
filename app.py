d3d78ab19a357b557359a1a866780a7b52442c25
Nar-n-dra-kumar <135616765+Nar-n-dra-kumar@users.noreply.github.com>
2024-06-20 06:34:59 +0000

    latest commits dynamic data

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 1,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 1,
+    'title': 'Frontend engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000'
+  },
+  {
+    'id': 1,
+    'title': 'Backend engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+  }
+]
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html',jobs=JOBS,company_name='Bob')
+  
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
